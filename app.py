0cb862883ebc3412179e1d53e0d8c7bf417c8e6a
rkt1234 <tiwari.ravikant2001@gmail.com>
2024-08-03 16:39:17 +0530

    "Removed CORS import and initialization from app.py"

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+
 
 db = SQLAlchemy()
 
 def createApp():
     app = Flask(__name__)
-    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://trail_owner:KC3FnYcO5xBp@ep-blue-scene-a5e7rn2t.us-east-2.aws.neon.tech/sample?sslmode=require'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'hytjo89@34'
